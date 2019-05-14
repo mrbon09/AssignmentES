6857dd5c1889fecae62346eb2b7bfc453139cbab
mrbon09 <mrbon09@users.noreply.github.com>
2019-05-14 23:59:08 +0700

    export to PDF done but cannot choose features to export

--- app/src/main/java/com/assignment/duy/assignmentes/ExportToPDF.java
+++ app/src/main/java/com/assignment/duy/assignmentes/ExportToPDF.java
@@ -1,6 +1,12 @@
 package com.assignment.duy.assignmentes;
 
+import android.os.Environment;
+import android.provider.DocumentsContract;
 import android.support.v4.app.Fragment;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -8,15 +14,44 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.lang.annotation.Documented;
 
 public class ExportToPDF extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.export_pdf, container, false);
+        View view = inflater.inflate(R.layout.export_pdf, container, false);
+        view.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createPDF();
+            }
+        });
+        return view;
     }
 
-    public void createPDF (View view) {
-
+    public void createPDF(){
+        //EditText txt = (EditText)getView().findViewById(R.id.choose_feature);
+        TextView head = (TextView)getView().findViewById(R.id.heading_s);
+        Paragraph paragraph1 = new Paragraph(head.getText().toString());
+        Document doc = new Document();
+        String outPath = Environment.getExternalStorageDirectory()+"/healthsheet.pdf";
+        try {
+            PdfWriter.getInstance(doc, new FileOutputStream(outPath));
+            doc.open();
+            doc.add(paragraph1);
+            doc.close();
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
+
+
 }
