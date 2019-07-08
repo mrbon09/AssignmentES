1dd0c06b2eeb289558ef7be1480f4856844d25af
bimupmup <42055607+bimupmup@users.noreply.github.com>
2019-07-08 14:25:52 +0700

    updated News

--- app/src/main/java/com/assignment/duy/assignmentes/NewsFragment.java
+++ app/src/main/java/com/assignment/duy/assignmentes/NewsFragment.java
@@ -1,5 +1,7 @@
 package com.assignment.duy.assignmentes;
 
+package com.assignment.duy.assignmentes;
+
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.graphics.Typeface;
@@ -19,17 +21,11 @@ import android.widget.TextView;
 
 
 public class NewsFragment extends Fragment {
-    Button b1,b2,b3,b4,b5;
+    Button b1,b2,b3,b4,b6,b7,b9,b10,search;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.news_layout, container, false); // call layout
-        TextView text= view.findViewById(R.id.editText3);
-        String t="WeLcoMe!!!!!!!";
-        SpannableString ss=new SpannableString(t);
-        StyleSpan boldIta=new StyleSpan(Typeface.BOLD_ITALIC);
-        ss.setSpan(boldIta,0,6, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        text.setText(ss);
+        View view = inflater.inflate(R.layout.news_layout, container, false);    //call layout
         return view;
     }
     public void onActivityCreated(@Nullable Bundle savedInstances){
@@ -37,40 +33,68 @@ public class NewsFragment extends Fragment {
         b1=getActivity().findViewById(R.id.button1);
         b1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent browser1 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.theguardian.com/books/2014/oct/22/being-mortal-medicine-what-matters-atul-gawande-review"));
+                Intent browser1 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://tuoitre.vn/suc-khoe.htm"));
                 startActivity(browser1);
             }
         });
         b2=getActivity().findViewById(R.id.button2);
         b2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent browser2 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.scientificamerican.com/article/book-review-pandemic/"));
+                Intent browser2 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://thanhnien.vn/suc-khoe/"));
                 startActivity(browser2);
             }
         });
         b3=getActivity().findViewById(R.id.button3);
         b3.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent browser3 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.huffpost.com/entry/book-review_n_3773370"));
+                Intent browser3 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://dantri.com.vn/suc-khoe.htm"));
                 startActivity(browser3);
             }
         });
         b4=getActivity().findViewById(R.id.button4);
         b4.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent browser4 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.nyjournalofbooks.com/book-review/" +
-                        "brave-new-world-healthcare-revisited-what-every-american-needs-know-about-our-healthcare"));
+                Intent browser4 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://vnexpress.net/suc-khoe"));
                 startActivity(browser4);
             }
         });
-        b5=getActivity().findViewById(R.id.button5);
-        b5.setOnClickListener(new View.OnClickListener() {
+        b6=getActivity().findViewById(R.id.button6);
+        b6.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent browser5 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.goodreads.com/book/show/763331.Flu"));
+                Intent browser5 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.bbc.com/news/health"));
                 startActivity(browser5);
             }
         });
+        b7=getActivity().findViewById(R.id.button7);
+        b7.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent browser5 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://edition.cnn.com/health"));
+                startActivity(browser5);
+            }
+        });
+
+        b9=getActivity().findViewById(R.id.button9);
+        b9.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent browser5 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.sciencedaily.com/news/top/health/"));
+                startActivity(browser5);
+            }
+        });
+        b10=getActivity().findViewById(R.id.button10);
+        b10.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent browser5 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.medicalnewstoday.com/"));
+                startActivity(browser5);
+            }
+        });
+        search=getActivity().findViewById(R.id.button5);
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent();
+                intent.setClassName("com.assignment.duy.assignmentes","com.assignment.duy.assignmentes.HealthSearch");
+                startActivity(intent);
+            }
+        });
     }
-
 }
-
